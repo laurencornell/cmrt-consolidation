27d458e8020ea4925de2822ff64ab871d8354697
laurencornell <laurenc@spectralogic.com>
2021-03-19 10:26:03 -0600

    checks approved smelters, writes to template

--- consolidate.py
+++ consolidate.py
@@ -1,33 +1,51 @@
-import csv
 import os
 import openpyxl
-from smelter import Smelter
 
 path = r"/home/lauren/Git/cmrt-consolidation/responses/"
+output_file = "RMI_CMRT_6.01.xlsx"
 au_list = {}
 sn_list = {}
 w_list = {}
 ta_list = {}
+metals = {"Gold", "Tin", "Tungsten", "Tantalum"}
+approved_smelters = []
+unapproved_smelters = []
+
+template = openpyxl.load_workbook(output_file, data_only=True)
+smelter_lookup = template["Smelter Look-up"]
+for smelter_row in smelter_lookup.rows:
+    if metals.__contains__(smelter_row[0].value):
+        if smelter_row[4].value is not None:
+            approved_smelters.append(smelter_row[4].value)
 
 for file in os.listdir(path):
     xlsx_sheet = openpyxl.load_workbook(path+file, data_only=True)["Smelter List"]
-    for r in xlsx_sheet.rows:
+    for r in xlsx_sheet.iter_rows():
         cid = r[0].value
-        if cid is not None:
-            smelter = Smelter(cid, r[1].value)
-            if smelter.metal == "Gold":
-                au_list[smelter.id_number] = smelter
-            elif smelter.metal == "Tin":
-                sn_list[smelter.id_number] = smelter
-            elif smelter.metal == "Tungsten":
-                w_list[smelter.id_number] = smelter
-            elif smelter.metal == "Tantalum":
-                ta_list[smelter.id_number] = smelter
+        if approved_smelters.__contains__(cid):
+            metal = r[1].value
+            if metal == "Gold":
+                au_list[cid] = r
+            elif metal == "Tin":
+                sn_list[cid] = r
+            elif metal == "Tungsten":
+                w_list[cid] = r
+            elif metal == "Tantalum":
+                ta_list[cid] = r
+        else:
+            unapproved_smelters.append(r)
 
 all_smelters = list(au_list.values()) + list(sn_list.values()) + list(w_list.values()) + list(ta_list.values())
 
-with open('all_smelters.csv', 'w', newline='') as final_file:
-    writer = csv.writer(final_file)
-    writer.writerow(['ID number', 'Metal'])
-    for smelter in all_smelters:
-        writer.writerow([smelter.id_number, smelter.metal])
+spectra_list = template["Smelter List"]
+smelter_count = 0
+row_count = 5
+
+for smelter in all_smelters:
+    cell_count = 1
+    for cell in smelter:
+        spectra_list.cell(row=row_count, column=cell_count).value = cell.value
+        cell_count += 1
+    row_count += 1
+
+template.save("Spectra.xlsx")
--- smelter.py
+++ smelter.py
@@ -1,7 +1,16 @@
 class Smelter(object):
-    def __init__(self, id_number, metal):
+    def __init__(self, id_number, metal, lookup, name, country, id_source, city, facility, contact_name, contact_email, next_steps, mine_names, mine_location, recycled_sources, comments):
         self.id_number = id_number
         self.metal = metal
+        self.lookup = lookup
+        self.name = name
+        self.country = country
+        self.id_source = id_source
+        self.city = city
+        self.facility = facility
+        self.contact_name = contact_name
+        self.contact_email = contact_email
+        self.next_steps
 
     def __repr__(self):
         return "ID: " + self.id_number + ", metal: " + self.metal

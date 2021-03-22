31ac54015256fbce2277b6a56ee10794219e7923
laurencornell <laurenc@spectralogic.com>
2021-03-22 10:53:07 -0600

    Catch BadZipFile exception

--- consolidate.py
+++ consolidate.py
@@ -1,4 +1,6 @@
 import os
+import zipfile
+
 import openpyxl
 import PySimpleGUI as sg
 
@@ -12,27 +14,38 @@ def consolidate(responses_folder, template_file, output_file):
                 approved_smelters.append(smelter_row[4].value)
 
     for file in os.listdir(responses_folder):
-        xlsx_sheet = openpyxl.load_workbook(os.path.join(responses_folder, file), data_only=True)["Smelter List"]
-        for r in xlsx_sheet.iter_rows():
-            cid = r[0].value
-            if approved_smelters.__contains__(cid):
-                metal = r[1].value
-                if metal == "Gold":
-                    au_list[cid] = r
-                elif metal == "Tin":
-                    sn_list[cid] = r
-                elif metal == "Tungsten":
-                    w_list[cid] = r
-                elif metal == "Tantalum":
-                    ta_list[cid] = r
-            else:
-                unapproved_smelters.append(r)
+        if file.endswith(".xlsx"):
+            unapproved_rows = []
+            try:
+                xlsx_sheet = openpyxl.load_workbook(os.path.join(responses_folder, file), data_only=True)["Smelter List"]
+                for r in xlsx_sheet.iter_rows(min_row=4, min_col=1):
+                    cid = r[0].value
+                    metal = r[1].value
+                    if cid is None and r[3].value is None:
+                        """Smelter will have one of these two values. Skip."""
+                    elif cid is not None and r[3].value is not None:
+                        """Smelter will only have one of these two values. Skip."""
+                    elif approved_smelters.__contains__(cid):
+                        if metal == "Gold":
+                            au_list[cid] = r
+                        elif metal == "Tin":
+                            sn_list[cid] = r
+                        elif metal == "Tungsten":
+                            w_list[cid] = r
+                        elif metal == "Tantalum":
+                            ta_list[cid] = r
+                    else:
+                        unapproved_rows.append(r)
+            except zipfile.BadZipFile:
+                print(file + " did not having any smelters in Smelter List. Please verify!")
+                """ignore"""
+
+            if len(unapproved_rows):
+                unapproved_smelters[file] = unapproved_rows
 
     all_smelters = list(au_list.values()) + list(sn_list.values()) + list(w_list.values()) + list(ta_list.values())
-
     spectra_list = template["Smelter List"]
     row_count = 5
-
     for smelter in all_smelters:
         cell_count = 1
         for cell in smelter:
@@ -40,7 +53,19 @@ def consolidate(responses_folder, template_file, output_file):
             cell_count += 1
         row_count += 1
 
-    template.save(output_file)
+    template.save(output_file+".xlsx")
+
+    if len(unapproved_smelters):
+        row_count = 1
+        unapproved_file = openpyxl.Workbook()
+        unapproved_sheet = unapproved_file.active
+        for file, bad_smelters in unapproved_smelters.items():
+            unapproved_sheet.cell(row=row_count, column=1, value=file)
+            print(file)
+            for bad_smelter in bad_smelters:
+                unapproved_sheet.cell(row=row_count, column=2, value=bad_smelter[0].value)
+                row_count += 1
+        unapproved_file.save("bad_smelters.xlsx")
 
 
 au_list = {}
@@ -49,21 +74,17 @@ w_list = {}
 ta_list = {}
 metals = {"Gold", "Tin", "Tungsten", "Tantalum"}
 approved_smelters = []
-unapproved_smelters = []
+unapproved_smelters = {}
 
 sg.theme('Reddit')
-layout = [[sg.T("")],
-          [sg.Text("Choose a responses folder: "), sg.Input(key="folder"), sg.FolderBrowse()],
-          [sg.Text("Choose a template: "), sg.Input(key="template"), sg.FileBrowse()],
-          [sg.Text("Enter output name: "), sg.Input(key="output"), sg.Text()],
+layout = [[sg.Text("Select responses folder: "), sg.Text(" "), sg.Input(key="folder"), sg.FolderBrowse()],
+          [sg.Text("Select the CMRT template: "), sg.Input(key="template"), sg.FileBrowse()],
+          [sg.Text("Save file as: "), sg.Text(" "*18), sg.Input(key="output"), sg.Text(".xlsx")],
           [sg.Button('Consolidate')]]
-window = sg.Window('Push', layout, size=(800, 300))
+window = sg.Window('Consolidate CMRT responses', layout, size=(600, 180))
 
 while True:
     event, values = window.read()
-    print(values["folder"])
-    print(values["template"])
-    print(values["output"])
     if event == sg.WIN_CLOSED:
         break
     elif event == 'Consolidate':
